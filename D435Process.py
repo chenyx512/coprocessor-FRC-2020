12b71f93461ea6294a7cd0e288b92ef15c6dcb63
chenyx512 <yuxuanchen@stmarksschool.org>
2020-01-21 23:28:14 -0500

    Tune depth to work a bit

--- D435Process.py
+++ D435Process.py
@@ -1,6 +1,7 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import math
 
 align_to = rs.stream.color
 align = rs.align(align_to)
@@ -14,14 +15,30 @@ class D435Process:
         cv2.imshow('img', img)
         cv2.waitKey(1)
 
+    def check(self, cnt):
+        cnt_area = cv2.contourArea(cnt)
+        if cnt_area < 200:
+            return False
+        _, radius = cv2.minEnclosingCircle(cnt)
+        ratio = cnt_area / math.pi * radius * radius
+        return ratio > 0.6
+
     def run(self):
         pipeline_d435 = rs.pipeline()
         config_d435 = rs.config()
         config_d435.enable_device('923322071945')
-        config_d435.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-        config_d435.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+        config_d435.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
+        config_d435.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 30)
         profile_d435 = pipeline_d435.start(config_d435)
 
+        depth_sensor = profile_d435.get_device().first_depth_sensor()
+        preset_range = depth_sensor.get_option_range(rs.option.visual_preset)
+        for i in range(int(preset_range.max)):
+            visulpreset = depth_sensor.get_option_value_description(
+                rs.option.visual_preset, i)
+            if visulpreset == 'Default':
+                print('set default')
+                depth_sensor.set_option(rs.option.visual_preset, i)
 
         try:
             while True:
@@ -38,35 +55,38 @@ class D435Process:
                 depth_image = np.asanyarray(depth_frame.get_data())
                 color_image = np.asanyarray(color_frame.get_data())
 
+                depth_image = cv2.blur(depth_image, (5, 5))
+                # depth_image = cv2.bilateralFilter(depth_image, 7, 50, 50)
+                sobelx = cv2.Sobel(depth_image, cv2.CV_16SC1, 1, 0, ksize=5)
+                sobely = cv2.Sobel(depth_image, cv2.CV_16SC1, 0, 1, ksize=5)
+                edges = cv2.Canny(sobelx, sobely, 2500, 5500)
 
-                laplacian = cv2.Laplacian(depth_image, cv2.CV_64F)
-                # self.show(laplacian)
-                # sobelx = cv2.Sobel(depth_image, cv2.CV_16SC1, 1, 0, ksize=5)
-                # sobely = cv2.Sobel(depth_image, cv2.CV_16SC1, 0, 1, ksize=5)
-                # edges = cv2.Canny(sobelx, sobely, 900, 900)
+                # depth_uint8 = np.maximum(255, depth_image.copy()/5).astype(np.uint8)
+                # depth_uint8 = cv2.bilateralFilter(depth_uint8, 7, 50, 50)
+                # edges = cv2.Canny(depth_uint8, 400, 1100)
 
-                depth_uint8 = np.maximum(255, depth_image.copy()/5).astype(np.uint8)
-                kernel = np.ones((7,7),np.uint8)
-                erosion = cv2.erode(depth_uint8, kernel=kernel, iterations = 4)
-                edges = cv2.Canny(erosion, 30, 120)
-                dilation = cv2.dilate(erosion, kernel=kernel, iterations = 4)
+                edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, np.ones((5,5),np.uint8))
                 contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 50]
-                cv2.drawContours(color_image, contours, -1, (0,255,0), 3)
-                
-                
+                contours = [cnt for cnt in contours if self.check(cnt)]
 
+                cv2.drawContours(color_image, contours, 0, (0,255,0), 3)
+                
                 edges = np.stack([edges, edges, edges], axis=-1)
                 # edges = np.where(edges==255, edges, color_image)
                 self.show(color_image)
 
                 # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-                depth_colormap = cv2.applyColorMap(
-                    cv2.convertScaleAbs(depth_image, alpha=0.03),
-                    cv2.COLORMAP_JET)
-
+                # depth_image = cv2.applyColorMap(
+                #     cv2.convertScaleAbs(depth_image, alpha=0.03),
+                #     cv2.COLORMAP_JET
+                # )
+                # depth_image_blur = cv2.applyColorMap(
+                #     cv2.convertScaleAbs(depth_image_blur, alpha=0.03),
+                #     cv2.COLORMAP_JET
+                # )
                 # Stack both images horizontally
-                images = np.hstack((color_image, depth_colormap))
+                # images = np.hstack((depth_image_blur, depth_image))
+                # self.show(images)
 
         finally:
             print('stop')
