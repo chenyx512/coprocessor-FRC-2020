9c0b6b917b238f70634cabe1f7fa8b603f0d4cc2
chenyx <yuxuanchen@stmarksschool.org>
2020-02-01 10:33:21 -0500

    Debug CameraServerWrapper

--- util/CameraServerWrapper.py
+++ util/CameraServerWrapper.py
@@ -1,4 +1,9 @@
 import logging
+try:
+    from cscore import CameraServer
+    WORKING = True
+except ModuleNotFoundError:
+    WORKING = False
 
 from Constants import Constants
 
@@ -10,18 +15,14 @@ class CameraServerWrapper:
     """
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        try:
-            from cscore import CameraServer
-            self.working = True
-            self.video_server = {}
-        except ModuleNotFoundError:
-            self.working = False
+        self.video_server = {}
+        if not WORKING:
             self.logger.warning('CameraServer not installed')
 
     def put_frame(self, name, frame):
-        if not self.working:
+        if not WORKING:
             return
         if not name in self.video_server:
             self.video_server[name] = CameraServer.getInstance().\
-                putVideo('frame', frame.shape[1], frame.shape[0])
+                putVideo(name, frame.shape[1], frame.shape[0])
         self.video_server[name].putFrame(frame)
