7127b102c6c71976e5c72a405c32ea65f414048a
chenyx512 <yuxuanchen@stmarksschool.org>
2020-02-25 10:48:19 -0500

    Enable hsv_tuner for both D435 and webcam

--- calibration_util/hsv_tuner.py
+++ calibration_util/hsv_tuner.py
@@ -1,11 +1,17 @@
 import cv2
 import argparse
 from operator import xor
+import pyrealsense2 as rs
+import numpy as np
 
 
 def callback(value):
     pass
 
+D435 = False
+DEPTH_H = 480
+DEPTH_W = 640
+FPS = 30
 
 def setup_trackbars(range_filter):
     cv2.namedWindow("Trackbars", 0)
@@ -30,18 +36,30 @@ def get_trackbar_values(range_filter):
 
 def main():
     range_filter = 'HSV'
-    setup_trackbars(range_filter)
-    cap = cv2.VideoCapture(0) # TODO change this for camera
-    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
-    cap.set(cv2.CAP_PROP_EXPOSURE, -9)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640);
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480);
+    if not D435:
+        setup_trackbars(range_filter)
+        cap = cv2.VideoCapture(0) # TODO change this for camera
+        cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
+        cap.set(cv2.CAP_PROP_EXPOSURE, -9)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640);
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480);
+    else:
+        pipeline_d435 = rs.pipeline()
+        config_d435 = rs.config()
+        config_d435.enable_device('923322071945')
+        config_d435.enable_stream(rs.stream.depth, DEPTH_W, DEPTH_H, rs.format.z16,
+                                  FPS)
+        config_d435.enable_stream(rs.stream.color, DEPTH_W, DEPTH_H,
+                                  rs.format.bgr8, FPS)
+        profile_d435 = pipeline_d435.start(config_d435)
 
     while True:
-        ret, image = cap.read()
-
-        if not ret:
-            break
+        if not D435:
+            ret, image = cap.read()
+        else:
+            frames = pipeline_d435.wait_for_frames()
+            image = frames.get_color_frame()
+            image = np.asanyarray(image.get_data())
 
         frame_to_thresh = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         v1_min, v2_min, v3_min, v1_max, v2_max, v3_max = get_trackbar_values(range_filter)
--- test.py
+++ test.py
@@ -1,4 +1,13 @@
+import multiprocessing as mp
+
+from T265Process import T265Process
 from D435Process import D435Process
 
-process = D435Process()
+
+frame_queue = mp.Queue(10)
+ball_queue = mp.Queue(1)
+xyzrpy_value = mp.Array('f', 6)
+
+process = D435Process(frame_queue, ball_queue, xyzrpy_value)
+# process = T265Process(frame_queue, frame_queue, frame_queue)
 process.run()
