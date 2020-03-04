046e85673fe6afa7a23205c9632f028342dd2221
chenyx512 <yuxuanchen@stmarksschool.org>
2020-03-03 21:30:22 -0500

    Fix target detection

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import multiprocessing as mp
+import logging
 
 from T265Process import T265Process
 from D435Process import D435Process
+from CVProcess import CVProcess
 
-
+encoder_v_queue = mp.Queue(1)
+xyz_rpy_queue = mp.Queue(1)
 frame_queue = mp.Queue(10)
-ball_queue = mp.Queue(1)
+ntable_queue = mp.Queue(100)
 xyzrpy_value = mp.Array('f', 6)
+target_queue = mp.Queue(1)
+ball_queue = mp.Queue(1)
 
-process = D435Process(frame_queue, ball_queue, xyzrpy_value)
+# process = D435Process(frame_queue, ball_queue, xyzrpy_value)
+process = CVProcess(target_queue, xyzrpy_value, frame_queue, ntable_queue)
+process.logger.setLevel(50)
 process.daemon = True
 # process = T265Process(frame_queue, frame_queue, frame_queue)
 process.run()
